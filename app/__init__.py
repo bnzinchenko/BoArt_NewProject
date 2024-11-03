093931b9f1d6e596b5e1b57696b883eb56b25b20
bnzinchenko <bnzinchenko90@gmail.com>
2024-11-03 22:45:34 +0300

    backend-ready, but no blueeprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, Config
 from .extensions import db
+from .config.py import Config
 
-
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+    app.config.from_object(config_class)
 
     db.init_app(app)
-
     with app.app_context():
         db.create_all()
 
