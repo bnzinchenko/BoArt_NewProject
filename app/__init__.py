4a99890aa76f0fa6129233b7749956f636cedbf5
bnzinchenko <bnzinchenko90@gmail.com>
2024-11-03 23:41:30 +0300

    backend-ready, but no blueeprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask, Config
 from .extensions import db
-from .config.py import Config
+from .config import Config
+
+
+from .routes.user import user
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    app.register_blueprint(user)
+
 
     db.init_app(app)
     with app.app_context():
