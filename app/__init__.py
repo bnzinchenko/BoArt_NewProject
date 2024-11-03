501fb8c438143ddfe07c16876aa64af734f69042
bnzinchenko <bnzinchenko90@gmail.com>
2024-11-03 22:11:37 +0300

    app initial almost ready

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
 from flask import Flask
+from .extensions import db
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
